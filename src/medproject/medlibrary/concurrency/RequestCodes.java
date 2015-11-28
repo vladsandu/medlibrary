d87ecab5ca5639980aeea5951d5743e59ba6100e
Vlad Sandu <vladsandu1994@gmail.com>
2015-11-28 23:51:06 +0200

    Add UpdatePatientAddress request

--- src/medproject/medlibrary/concurrency/RequestCodes.java
+++ src/medproject/medlibrary/concurrency/RequestCodes.java
@@ -12,13 +12,15 @@ public class RequestCodes {
 	public static final int PATIENT_LIST_REQUEST = 2;
 	public static final int PATIENT_RECORD_BY_CNP_REQUEST = 3;
 	public static final int ADD_PATIENT_REQUEST = 4;
+	public static final int UPDATE_PATIENT_ADDRESS_REQUEST = 5;
 	
 	public static int getRequestType(Request request){
 		if(request.getREQUEST_CODE() == LOGIN_REQUEST)
 			return LOGIN_TYPE_REQUEST;
 		else if(request.getREQUEST_CODE() == PATIENT_LIST_REQUEST ||
 				request.getREQUEST_CODE() == PATIENT_RECORD_BY_CNP_REQUEST ||
-				request.getREQUEST_CODE() == ADD_PATIENT_REQUEST)
+				request.getREQUEST_CODE() == ADD_PATIENT_REQUEST ||
+				request.getREQUEST_CODE() == UPDATE_PATIENT_ADDRESS_REQUEST)
 			return PATIENT_TYPE_REQUEST;
 		return -1;
 	}
