b5291afcae9d18f56ab6ee47a0c875599118fb2e
Vlad Sandu <vladsandu1994@gmail.com>
2015-11-28 23:50:07 +0200

    Add status to CustomTask

--- src/medproject/medlibrary/concurrency/CustomTask.java
+++ src/medproject/medlibrary/concurrency/CustomTask.java
@@ -8,11 +8,13 @@ public abstract class CustomTask implements Runnable{
 
 	protected Object data;
 	private final int requestCode;
+	protected RequestStatus requestStatus;
 	private final CountDownLatch latch;
 	
 	public CustomTask(int requestCode) {
 		this.requestCode = requestCode;
 		this.latch = new CountDownLatch(1);
+		this.requestStatus = RequestStatus.REQUEST_PENDING;
 	}
 
 	public void setData(Object data) {
@@ -26,6 +28,12 @@ public abstract class CustomTask implements Runnable{
 	public CountDownLatch getLatch() {
 		return latch;
 	}
-	
-	
+
+	public RequestStatus getRequestStatus() {
+		return requestStatus;
+	}
+
+	public void setRequestStatus(RequestStatus requestStatus) {
+		this.requestStatus = requestStatus;
+	}
 }
